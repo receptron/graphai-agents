f1125995390825a5df386de3acb58060b9266ad8
snakajima <satoshi.nakajima@gmail.com>
2025-04-05 09:55:30 -0700

    createMcpToolsCallAgent

--- protocol/mcp-agent/src/mcp_tools_call_agent.ts
+++ protocol/mcp-agent/src/mcp_tools_call_agent.ts
@@ -1,47 +1,51 @@
 import { AgentFunction, AgentFunctionInfo } from "graphai";
-import { toolsCall } from "./mcp";
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { createToolsCall } from "./mcp";
 
-export const mcpToolsCallAgent: AgentFunction = async ({ namedInputs }) => {
-  const { name, arguments: mcpArguments } = namedInputs.tools;
-  const response = await toolsCall({ name, arguments: mcpArguments });
-  return {
-    response,
+export const createMcpToolsCallAgent = (mcpClents: Record<string, Client>) => {
+  const toolsCall = createToolsCall(mcpClents);
+  return async ({ namedInputs }: { namedInputs: any }) => {
+    const { name, arguments: mcpArguments } = namedInputs.tools;
+    const response = await toolsCall({ name, arguments: mcpArguments });
+    return {
+      response,
+    };
   };
 };
 
-const mcpToolsCallAgentInfo: AgentFunctionInfo = {
-  name: "mcpToolsCallAgent",
-  agent: mcpToolsCallAgent,
-  mock: mcpToolsCallAgent,
+export const createMcpToolsCallAgentInfo = (mcpToolsCallAgent: AgentFunction) => {
+  return {
+    name: "mcpToolsCallAgent",
+    agent: mcpToolsCallAgent,
+    mock: mcpToolsCallAgent,
 
-  samples: [
-    {
-      params: {},
-      inputs: {
-        tools: {
-          name: "filesystem--list_directory",
-          arguments: {
-            path: __dirname + "/../tests/sample",
+    samples: [
+      {
+        params: {},
+        inputs: {
+          tools: {
+            name: "filesystem--list_directory",
+            arguments: {
+              path: __dirname + "/../tests/sample",
+            },
+          },
+        },
+        result: {
+          response: {
+            content: [
+              {
+                text: "[FILE] 1.txt\n[FILE] 2.txt",
+                type: "text",
+              },
+            ],
           },
         },
       },
-      result: {
-        response: {
-          content: [
-            {
-              text: "[FILE] 1.txt\n[FILE] 2.txt",
-              type: "text",
-            },
-          ],
-        },
-      },
-    },
-  ],
-  description: "Model Context Protocol Tools/Call Agent",
-  category: ["protocol"],
-  author: "isamu arimoto",
-  repository: "https://github.com/receptron/graphai-agents",
-  license: "MIT",
+    ],
+    description: "Model Context Protocol Tools/Call Agent",
+    category: ["protocol"],
+    author: "isamu arimoto",
+    repository: "https://github.com/receptron/graphai-agents",
+    license: "MIT",
+  };
 };
-
-export default mcpToolsCallAgentInfo;
