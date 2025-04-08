9ae844a506392c95906603ffd67af65f0124a719
isamu <isamu@to-kyo.to>
2025-04-08 11:08:03 +0900

    add test

---
+++ packages/all/tests/run.ts
@@ -0,0 +1,25 @@
+
+// import { GraphAI } from "@graphai/all";
+import { GraphAI, agents } from "../src/";
+
+
+const main = async () => {
+  const graphData = {
+    version: 0.5,
+    nodes: {
+      init: { value: "hello" },
+      result: {
+        agent: "copyAgent",
+        inputs: {
+          text: ":init",
+        },
+        isResult: true,
+      },
+    },
+  };
+  const graphai = new GraphAI(graphData, agents);
+  const result = await graphai.run();
+  console.log(result);
+};
+
+main();
