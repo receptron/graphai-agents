3c4d9461bf681a6627cf5dab401752ef825317ee
isamu <isamu@to-kyo.to>
2025-05-06 09:37:29 +0900

    fix

--- voice/tts-nijivoice-agent/tests/run_agent.ts
+++ voice/tts-nijivoice-agent/tests/run_agent.ts
@@ -7,7 +7,12 @@ const main = async () => {
   const namedInputs = { text: "こんにちは" };
   const params = { voice: "b9277ce3-ba1c-4f6f-9a65-c05ca102ded0" };
 
-  const res = (await ttsNijivoiceAgent({ namedInputs, params, filterParams: {}, debugInfo: { verbose: false, nodeId: "test", retry: 5 } })) as any;
+  const res = (await ttsNijivoiceAgent({
+    namedInputs,
+    params,
+    filterParams: {},
+    debugInfo: { verbose: false, nodeId: "test", retry: 5, subGraphs: new Map(), state: "" },
+  })) as any;
   const filePath = path.resolve("./test.mp3");
   await fs.promises.writeFile(filePath, res.buffer);
 };
--- voice/tts-openai-agent/tests/run_agent.ts
+++ voice/tts-openai-agent/tests/run_agent.ts
@@ -7,7 +7,12 @@ const main = async () => {
   const namedInputs = { text: "hello, this is pen" };
   const params = {};
 
-  const res = (await ttsOpenaiAgent({ namedInputs, params, filterParams: {}, debugInfo: { verbose: false, nodeId: "test", retry: 5, subGraphs: new Map(), state: "" },  })) as any;
+  const res = (await ttsOpenaiAgent({
+    namedInputs,
+    params,
+    filterParams: {},
+    debugInfo: { verbose: false, nodeId: "test", retry: 5, subGraphs: new Map(), state: "" },
+  })) as any;
   const filePath = path.resolve("./test.mp3");
   await fs.promises.writeFile(filePath, res.buffer);
 };
